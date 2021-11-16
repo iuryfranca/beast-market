d9d2dc51676cdd83d59aa7a5bac7c39d0efea146
iury.franca <iurygfranca@gmail.com>
2021-11-16 01:08:58 -0400

    build: Creating Header and Adding chakra ✨

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,19 @@
-export function Header(){
+import styles from './styles.module.scss'
+
+export function Header() {
   return (
-    <div>
-      Beast market
-    </div>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <nav>
+          <h2>Beast Market</h2>
+        </nav>
+        <nav>
+          <a>Store</a>
+          <a>My Account</a>
+        </nav>
+
+        <button type="button">Teste</button>
+      </div>
+    </header>
   );
 }
